b282687237b7df7d81876b30b8999e32f279a7a4
Ubuntu <ubuntu@ip-172-31-39-155.us-west-2.compute.internal>
2017-01-11 12:05:33 +0000

    colores

--- src/modules/wallet/styles/Sending.js
+++ src/modules/wallet/styles/Sending.js
@@ -7,7 +7,7 @@ const styles = StyleSheet.create({
 	},
 	data_part : {
     fontFamily : 'roboto_normal',
-    color      : '#F64D27',
+    color      : '#B7F072',
     fontSize   : 25
   },
   title_part : {
